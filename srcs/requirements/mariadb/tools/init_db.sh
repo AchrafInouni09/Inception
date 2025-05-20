200b64c5cad851148b5fd644a8eb668ce4dfe82c
AchrafInouni09 <achrafinouni10@gmail.com>
2025-05-20 20:45:50 +0100

    fix init mariadb .sh

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -2,14 +2,37 @@
 
 echo "script of initing db is starting"
 
+mkdir -p /run/mysqld
+mkdir -p /var/mysqld
+
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+
+mkdir -p /var/mariadb
+chown -R mysql:mysql /var/mariadb
+
+
+
 if [ ! -d "/var/lib/mariadb" ]; then
     echo "maria shoud be initialized"
 
-    maria_install_db --user=mysql
+    mysql_install_db --user=mysql --datadir=/var/mariadb
 
-    maria --user=mysql << EOF
+    mysql --user=mysql --datadir=/var/mariadb <<EOF
+
+
 
 use mysql;
-
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
 EOF
+    echo "maria db initialized"
+else
+    echo "maria db already initialized"
 fi
+
+exec "$@"
