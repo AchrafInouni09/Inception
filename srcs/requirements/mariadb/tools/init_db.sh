feaef0f802b19f5041d4ea5541b1a8db95e0f718
AchrafInouni09 <achrafinouni10@gmail.com>
2025-05-23 20:22:28 +0100

    fix user creating init_db.sh

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -6,26 +6,22 @@ mkdir -p /run/mysqld
 mkdir -p /var/mysqld
 
 chown -R mysql:mysql /run/mysqld
-chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /var/mysqld
 
 mkdir -p /var/mariadb
 chown -R mysql:mysql /var/mariadb
 
 
-
-if [ ! -d "/var/lib/mariadb" ]; then
+if [ ! -d "/var/mariadb/mysql" ]; then
     echo "maria shoud be initialized"
 
     mysql_install_db --user=mysql --datadir=/var/mariadb
 
-    mysql --user=mysql --datadir=/var/mariadb <<EOF
-
-
-
+    mysqld --user=mysql --datadir=/var/mariadb --bootstrap <<EOF
 use mysql;
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
